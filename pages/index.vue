35474749bd03e2863a36460fb9fb16cc41463227
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2024-05-22 09:52:40 +1000

    feat: creates a simple landing page

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 class="text-4xl font-bold text-blue-600 mb-4">Welcome to XYZ Shopping</h1>
+      <nuxt-link to="/products" class="text-lg text-white bg-blue-500 hover:bg-blue-700 rounded-lg px-4 py-2">
+        View Products
+      </nuxt-link>
+    </div>
+  </template>
