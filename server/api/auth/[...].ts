b133efb914e3a0e566fac893fef773249fbe026b
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2024-05-23 16:14:28 +1000

    setup env variables

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,8 +5,8 @@ export default NuxtAuthHandler({
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GithubProvider.default({
-           clientId: 'Ov23liQ0Ff9e2Ji57dIS',
-           clientSecret: '0c2aaf94ea5b94c18cbaf74de8a6a176695ae65d'
+           clientId: process.env.CLIENT_ID,
+           clientSecret: process.env.CLIENT_SECRET
         })
     ]
 })
