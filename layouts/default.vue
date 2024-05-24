ba100a488e887d31116083e84ada3524d3e7ed39
Abhishekh Maharjan <abhishekmhrzn.14@gmail.com>
2024-05-24 10:17:55 +1000

    refactor: fix header width

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,8 +4,8 @@
 </script>
 
 <template>
-    <div class="max-w-6xl m-auto">
-        <div class="flex justify-between">
+    <div class="">
+        <div class="flex justify-between max-w-6xl m-auto">
             <div class="pt-3">
                 <nuxt-link :to="'/'" class="hover:text-blue-600 text-right">
                     <img src="/img/xxyyzz.jpeg" alt="Product Image" class=" h-12 w-20"/>
